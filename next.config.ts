2cd79b061620873c3752b3821eff5263873ebda6
GeorgiosKitsios <info@gkpokale.de>
2026-04-16 21:23:01 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  headers: async () => [
+    {
+      source: '/_next/static/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
